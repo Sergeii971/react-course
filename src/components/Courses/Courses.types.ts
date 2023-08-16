0cae91089d33d2b8926c463602e3d0a7978111e0
Sergeii971 <sergeiverbovskiy4@gmail.com>
2023-08-16 11:31:13 +0300

    complete first module

---
+++ src/components/Courses/Courses.types.ts
@@ -0,0 +1,6 @@
+import { CourseArray } from 'src/course.type';
+
+export interface CoursesProp {
+	courses: CourseArray;
+	searchLineValue: string;
+}
---
+++ src/course.type.ts
@@ -0,0 +1,10 @@
+export interface Course {
+	id: string;
+	title: string;
+	description: string;
+	creationDate: string;
+	duration: number;
+	authors: string[];
+}
+
+export type CourseArray = Course[];
